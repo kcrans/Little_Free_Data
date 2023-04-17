3dc8aabac4b8744ec7ef26bffc818ce609f18446
kaleb <kacrans@ucdavis.edu>
2023-04-17 10:23:18 -0700

    Added API data

--- get_more_data.py
+++ get_more_data.py
@@ -10,7 +10,6 @@ with open("locations.csv", newline = "") as f:
 		writer = csv.writer(new_f)
 		header = ["id", "Name", "Street__c", "City__c", "State_Province_Region__c", "Postal_Zip_Code__c", "Country__c", "Traveling_Library__c", "Official_Charter_Number__c", "First_Map_Date__c", "Map_Me__c", "Map_Date__c", "Duplicate_Charter_Number__c", "Count_of_Primary_Stewards__c", "Latitude_MapAnything__c", "Longitude_MapAnything__c", "Library_Geolocation__Latitude__s",  "Library_Geolocation__Longitude__s", "check_in_count"]
 		writer.writerow(header)
-		count = 0
 		for lib in reader:
 			# Take id number and look up expanded info
 			id = lib[0]
@@ -24,7 +23,4 @@ with open("locations.csv", newline = "") as f:
 			data = response.json()
 			newrow = [data[col] for col in header]
 			writer.writerow(newrow)
-			count += 1
-			if count == 10:
-				break
 
